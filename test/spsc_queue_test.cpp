6854d0c87e85c27f625af6182b8812cad2cdf7f8
Rahul Salvi <rsalvi006@me.com>
2018-09-02 00:24:40 -0700

    add multithreaded tests and tsan test

--- test/spsc_queue_test.cpp
+++ test/spsc_queue_test.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <thread>
+
 #include "spsc_queue.h"
 
 TEST(SPSC, SizePrimitive) {
@@ -40,4 +42,45 @@ TEST(SPSC, EnqueueAndDequeuePrimitive) {
         ASSERT_EQ(x, dequeue_ct);
     }
     ASSERT_EQ(test_queue.empty(), true);
+}
+
+// clang-format off
+#define MT_SMALL_QUEUE_SIZE 1
+#define MT_QUEUE_SIZE       1000
+#define MT_ITERATIONS       10000000
+// clang-format on
+
+template <size_t QUEUE_SIZE, size_t ITERATIONS>
+void enqueue_primitive_helper(rutils::spsc_queue<int, QUEUE_SIZE>* queue) {
+    for (int i = 0; i < ITERATIONS; i++) { queue->enqueue(i); }
+}
+
+template <size_t QUEUE_SIZE, size_t ITERATIONS>
+void dequeue_primitive_helper(rutils::spsc_queue<int, QUEUE_SIZE>* queue) {
+    for (int i = 0; i < ITERATIONS; i++) {
+        int x;
+        queue->dequeue(x);
+        ASSERT_EQ(x, i);
+    }
+    ASSERT_EQ(queue->empty(), true);
+}
+
+TEST(SPSC, EnqueueAndDequeuePrimitiveMT) {
+    int                                    buf[MT_QUEUE_SIZE];
+    rutils::spsc_queue<int, MT_QUEUE_SIZE> queue(buf);
+    std::thread enqueue_thread(enqueue_primitive_helper<MT_QUEUE_SIZE, MT_ITERATIONS>, &queue);
+    std::thread dequeue_thread(dequeue_primitive_helper<MT_QUEUE_SIZE, MT_ITERATIONS>, &queue);
+    enqueue_thread.join();
+    dequeue_thread.join();
+}
+
+TEST(SPSC, EnqueueAndDequeuePrimitiveMTSmall) {
+    int                                          buf[MT_SMALL_QUEUE_SIZE];
+    rutils::spsc_queue<int, MT_SMALL_QUEUE_SIZE> queue(buf);
+    std::thread enqueue_thread(enqueue_primitive_helper<MT_SMALL_QUEUE_SIZE, MT_ITERATIONS>,
+                               &queue);
+    std::thread dequeue_thread(dequeue_primitive_helper<MT_SMALL_QUEUE_SIZE, MT_ITERATIONS>,
+                               &queue);
+    enqueue_thread.join();
+    dequeue_thread.join();
 }
