67ff928cb6271801087cf6e4257fc040b169d79a
Rahul Salvi <rsalvi006@me.com>
2018-09-01 19:37:48 -0700

    enqueue and dequeue benchmarks

--- benchmark/spsc_queue_benchmark.cpp
+++ benchmark/spsc_queue_benchmark.cpp
@@ -2,10 +2,47 @@
 
 #include "spsc_queue.h"
 
-static void BM_BaseCase(benchmark::State& state) {
-    rutils::spsc_queue<int, 10> test_queue(NULL);
-    int                         x = 0;
-    for (auto _ : state) { test_queue.enqueue(x); }
+// clang-format off
+#define ENQUEUE_ITERATIONS       1000000
+#define ENQUEUE_REPETITIONS      50
+#define DEQUEUE_ITERATIONS       1000000
+#define DEQUEUE_REPETITIONS      50
+// clang-format on
+
+static void BM_Enqueue(benchmark::State& state) {
+    int                                         buf[ENQUEUE_ITERATIONS];
+    int                                         x = 0;
+    rutils::spsc_queue<int, ENQUEUE_ITERATIONS> test_queue(buf);
+    for (auto _ : state) {
+        test_queue.enqueue(x);
+        benchmark::ClobberMemory();
+    }
 }
 
-BENCHMARK(BM_BaseCase);
+BENCHMARK(BM_Enqueue)
+    ->Iterations(ENQUEUE_ITERATIONS)
+    ->Repetitions(ENQUEUE_REPETITIONS)
+    ->ReportAggregatesOnly(true)
+    ->ComputeStatistics("max", [](const std::vector<double>& v) -> double {
+        return *(std::max_element(std::begin(v), std::end(v)));
+    });
+
+static void BM_Dequeue(benchmark::State& state) {
+    int                                         buf[DEQUEUE_ITERATIONS];
+    int                                         x = 0;
+    rutils::spsc_queue<int, DEQUEUE_ITERATIONS> test_queue(buf);
+    for (int i = 0; i < DEQUEUE_ITERATIONS; i++) { test_queue.enqueue(x); }
+    for (auto _ : state) {
+        int x;
+        test_queue.dequeue(x);
+        benchmark::ClobberMemory();
+    }
+}
+
+BENCHMARK(BM_Dequeue)
+    ->Iterations(DEQUEUE_ITERATIONS)
+    ->Repetitions(DEQUEUE_REPETITIONS)
+    ->ReportAggregatesOnly(true)
+    ->ComputeStatistics("max", [](const std::vector<double>& v) -> double {
+        return *(std::max_element(std::begin(v), std::end(v)));
+    });
